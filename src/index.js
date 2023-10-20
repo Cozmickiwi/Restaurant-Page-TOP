3d523e8b893f72503098653e45a9e01d2dc62119
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-20 17:29:43 +1300

    set up css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+import './style.css';
+
 console.log('Hello World!!');
 
 function component() {
     const container = document.getElementById('content');
-    const test = document.createElement('p');
-    test.textContent = 'Hey!';
-    container.appendChild(test);
+    //header element
+    function header(){
+        const headerContainer = document.createElement('div');
+        const test = document.createElement('p');
+        test.textContent = 'test';
+        headerContainer.appendChild(test);
+        headerContainer.className = 'header';
+        return(headerContainer);
+    }
+
+    function body(){
+
+    }
+    container.appendChild(header());
     return(container)
 }
 document.body.appendChild(component());
